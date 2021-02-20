e8a8a1d497dcdd6fb4a0c64fc466da326463c6c5
heeo-8986 <heeo.8986@kakaocorp.com>
2021-02-20 23:31:56 +0900

    함수화/ 버튼 한 번만 클릭되게 수정

--- src/Elevator/index.js
+++ src/Elevator/index.js
@@ -1,12 +1,13 @@
-/*  
-  * @param {} floors - 층수 리스트
-  * @param {} buttons - 층수 버튼 리스트
-  * @param {} elevator - 엘레베이터
-  * @param {} movedElevator - 복제된 엘레베이터(새로 생성되며 움직이는 역할 하는 엘레베이터)
-  * @param {} textFloor - 나의 현재 층수 알림 텍스트
-  * @param {} clickedBtn - 가려고 누른 층 수
-  * @param {} idxOfclickedBtn - 가려고 누른 층 수의 실제 인덱스 값
-  * @param {} asIsFloorIdx - 현재 엘레베이터가있는 층의 실제 인덱스
+/**  
+  * @param {object} floors - 층 리스트
+  * @param {object} buttons - 층수 버튼 리스트
+  * @param {object} liftHoles - 엘레베이터가 위치한 층을 알아내기 위한 층(lift hole)리스트
+  * @param {object} elevator - 엘레베이터
+  * @param {object} movedElevator - 복제된 엘레베이터(새로 생성되며 움직이는 역할 하는 엘레베이터)
+  * @param {object} textFloor - 나의 현재 층수 알림 텍스트
+  * @param {object} clickedBtn - 가려고 누른 층 버튼
+  * @param {number} clickedFloor - 가려고 누른 층 수
+  * @param {number} idxOfclickedBtn - 가려고 누른 층 수의 실제 인덱스 값
  */
 
 let floors = [...document.getElementsByClassName('building')[0].getElementsByTagName('li')].slice().reverse();
@@ -15,20 +16,24 @@ let buttons = [...document.getElementsByClassName('elevator_floor_buttons')[0].g
 const elevator = document.querySelector('.elevator');
 const movedElevator = document.importNode(elevator,true);
 
-buttons.forEach((b, idx) => {
-  b.addEventListener('click', (event) => {
-    const clickedBtn = event.target;
-    const clickedFloor = Number.parseInt(clickedBtn.innerText);
-    const idxOfclickedBtn = idx;
-    
-    // 누른 버튼 color
-    buttons.map(b => b.querySelector('button').setAttribute('style','background: none'));
-    clickedBtn.closest('li').querySelector('button').setAttribute('style','background:tomato');
-
-    console.log(`${clickedFloor}층 누름`);
-    
-    // 엘레베이터 이동
-    moveElevator(clickedFloor,idxOfclickedBtn);
+const callElevator = (() => {
+  buttons.forEach((b, idx) => {
+    b.addEventListener('click', (event) => {
+      const clickedBtn = event.target;
+      const clickedFloor = Number.parseInt(clickedBtn.innerText);
+      const idxOfclickedBtn = idx;
+      
+      // 누른 버튼 color
+      buttons.map(b => b.querySelector('button').setAttribute('style','background: none'));
+      clickedBtn.closest('li').querySelector('button').setAttribute('style','background:tomato');
+  
+      console.log(`${clickedFloor}층 누름`);
+      
+      // 엘레베이터 이동
+      moveElevator(clickedFloor,idxOfclickedBtn);
+    }, {
+      once: true,
+    });
   });
 });
 
@@ -42,6 +47,7 @@ const findElevator = (() => {
     if(l.querySelector('.elevator')){
       console.log(`엘레베이터는 현재 ${idx+1}층에 있음`);
       currentElevatorFloor = idx;
+      return;
     } else {
       // console.log('현재 층에 엘레베이터가 없음 ');
     }
@@ -74,15 +80,16 @@ const moveElevator = ((clickdFloor, idxOfclickedBtn) => {
         elevator.remove();
         floors[count].querySelector('.lift_hole').append(movedElevator);
       
-        if(count+1 === clickdFloor){        
-          textFloor.innerHTML = `${count}층`;
+        if(count+1 === clickdFloor){  
           console.log(`[도착!!!]  count : ${count}, 누른층 : ${clickdFloor}`);
           clearInterval(moveUp);
         }  
         count++;
-        textFloor.innerHTML = `${count}층`;
+        textFloor.innerHTML = `${count}층`;  
     }, 700);
   } else {
     console.log('현재 층에 엘레베이터가 있음');
-  }  
+  }
 });
+
+callElevator();
